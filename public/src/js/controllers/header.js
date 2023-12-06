9214c4a02b6075f56d45b5eb362e56865e9321ce
User <mail@exaple.com>
2023-12-06 03:15:08 -0500

    fix color and disable charts

--- public/src/js/controllers/header.js
+++ public/src/js/controllers/header.js
@@ -16,9 +16,9 @@ angular.module('insight.system').controller('HeaderController',
       'title': 'Blocks',
       'link': 'blocks'
     }, {
-      'title': 'Charts',
-      'link': 'charts'
-    }, {
+    //   'title': 'Charts',
+    //   'link': 'charts'
+    // }, {
       'title': 'Status',
       'link': 'status'
     }];
